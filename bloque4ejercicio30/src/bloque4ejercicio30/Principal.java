6a33fb71c867ce6d2d4e8a3267fb5ede62723635
LAPTOP-VDKPLSA0\Oscar <oscarvillalba939@gmail.com>
2017-11-24 13:02:30 +0100

    Subimos

--- bloque4ejercicio30/src/bloque4ejercicio30/Principal.java
+++ bloque4ejercicio30/src/bloque4ejercicio30/Principal.java
@@ -4,12 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-alo ola bb
-=======
-adios bb
->>>>>>> origin/master
-
+ael
 	}
 
 }
